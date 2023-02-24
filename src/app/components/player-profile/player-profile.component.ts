d192cff27a06e01530a929fdca5425a30036e783
Eduardo Diniz <diniz480@gmail.com>
2023-02-24 14:37:33 -0300

    att

--- src/app/components/player-profile/player-profile.component.ts
+++ src/app/components/player-profile/player-profile.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PlayerProfileComponent implements OnInit {
 
-url:string = "/assets/images/";
+url:string = "/angular-game/assets/images/";
 @Input()
 profile:string = '';
 
